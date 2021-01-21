6189e0c728b40923b45a332f6052d846f5750d90
MaraPrazzy <61703707+MaraPrazzy@users.noreply.github.com>
2021-01-21 05:16:23 -0500

    added new table

---
+++ Sample_Copy/SalesLT/Tables/Address_Copy.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [SalesLT].[Address_Copy] (
+    [AddressID]        INT              IDENTITY (1, 1) NOT NULL,
+    [AddressLine1]     NVARCHAR (60)    NOT NULL,
+    [AddressLine2]     NVARCHAR (60)    NULL,
+    [City]             NVARCHAR (30)    NOT NULL,
+    [StateProvince]    [dbo].[Name]     NOT NULL,
+    [CountryRegion]    [dbo].[Name]     NOT NULL,
+    [PostalCode]       NVARCHAR (15)    NOT NULL,
+    [rowguid_new]      UNIQUEIDENTIFIER CONSTRAINT [DF_Address_rowguid_new] DEFAULT (newid()) NOT NULL,
+    [ModifiedDate_new] DATETIME         CONSTRAINT [DF_Address_ModifiedDate_new] DEFAULT (getdate()) NOT NULL,
+    [NewColumn1]       NCHAR (10)       NULL,
+    [Newcolumn2]       NCHAR (10)       NULL,
+    [SourceCol1]       NCHAR (10)       NULL,
+    [SourceCol2]       NCHAR (10)       NULL,
+    CONSTRAINT [PK_Address_AddressID_new] PRIMARY KEY CLUSTERED ([AddressID] ASC),
+    CONSTRAINT [AK_Address_rowguid_New] UNIQUE NONCLUSTERED ([rowguid_new] ASC)
+);
+
